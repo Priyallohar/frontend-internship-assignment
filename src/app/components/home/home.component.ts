c7680b6580242c0f199662c191b95f957f72e068
Priyal <mukul.malviya@orbis-vis.com>
2023-04-13 12:09:54 +0530

    solved the issue of not getting result by author name

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -41,33 +41,38 @@ export class HomeComponent implements OnInit {
         debounceTime(300),
       )
       .subscribe((value: string) => {
+        let bookSearchResponse;
+        let authorSearchResponse;
   
         this.http.get(`https://openlibrary.org/search.json?q=${value}`)
           .subscribe((response: any) => {
-            this.searchResults = response.docs.map((book: any) => ({
+            bookSearchResponse = response;
+            const bookResults = bookSearchResponse.docs.map((book: any) => ({
               title: book.title,
               key: book.key,
               authors: book.author_name?.map((name: string) => ({ name })) || [],
               first_publish_year: book.first_publish_year,
               author_name: book.author_name?.join(", ") || "Unknown"
             }));
-            this.tableView.booksList = this.searchResults;
+            this.tableView.booksList = [...bookResults, ...this.searchResults];
           });
   
         this.http.get(`https://openlibrary.org/search/authors.json?q=${value}`)
           .subscribe((response: any) => {
-            this.searchResults = response.docs.map((book: any) => ({
+            authorSearchResponse = response;
+            const authorResults = authorSearchResponse.docs.map((book: any) => ({
               title: book.top_work,
               key: book.key,
               authors: [{name:book.name}],
               first_publish_year: book.first_publish_year || 1899,
               author_name: book.name || "Unknown"
             }));
-             //this.tableView.booksList = this.searchResults;
-           this.tableView.booksList = [...this.tableView.booksList, ...this.searchResults]
+            this.searchResults = authorResults;
+            this.tableView.booksList = [...authorResults, ...this.searchResults];
           });
       });
   }
+
 }  
 
           
